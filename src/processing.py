b412e42d7637def8b44d4476591b2dea683166c0
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-04 02:41:21 +1000

    add test_processing

--- src/processing.py
+++ src/processing.py
@@ -7,24 +7,14 @@ def filter_by_state(data: list[dict[str, Any]], user_state: str = "EXECUTED") ->
     for i in data:
         if i["state"] == user_state:
             sorted_list.append(i)
+
+    #if user_state not in data[1]["state"]:
+    #    raise ValueError("Ключ не найден!")
+
     return sorted_list
 
 
 def sort_by_date(data: list[dict[str, Any]], descending: bool = True) -> list[dict[str, Any]]:
-    """Функция сортировки списка словарей по дате (по умолчанию - убывание)"""
+    """Функция сортировки списка банковских операций по дате (по умолчанию - убывание)"""
     sorted_list = sorted(data, key=lambda x: x["date"], reverse=descending)
     return sorted_list
-
-
-new_data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-if __name__ == '__main__':
-    print(filter_by_state(new_data))
-    print(filter_by_state(new_data, "CANCELED"))
-    print(sort_by_date(new_data, descending=False))
-    print(sort_by_date(new_data))
