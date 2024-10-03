42ced37fc50aab4549189bff0e2f0f316dbcd3a9
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-03 22:35:37 +1000

    fixes and init test_masks

--- src/masks.py
+++ src/masks.py
@@ -4,12 +4,13 @@ def get_mask_card_number(card_number: int) -> str:
     if mask_number.isdigit() and 17 > len(mask_number) > 15:
         return f"{mask_number[:4]} {mask_number[4:6]}** **** {mask_number[12:]}"
     else:
-        warning_message: str = "Ошибка ввода! Пожалуйста, введите 16-значный номер карты."
-        return warning_message
+        raise ValueError("Ошибка ввода! Пожалуйста, введите 16-значный номер карты.")
 
 
 def get_mask_account(account_number: int) -> str:
     """Функция маскировки номера банковского счета"""
     mask_account = str(account_number)
-    return f"**{mask_account[-4:]}"
-
+    if mask_account.isdigit() and len(mask_account) >= 20:
+        return f"**{mask_account[-4:]}"
+    else:
+        raise ValueError("Ошибка ввода! Пожалуйста, введите корректный номер счета (не менее 20 цифр)")
