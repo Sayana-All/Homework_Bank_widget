38f365010812d0ae357222ee540b8d27364cd269
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-04 04:19:53 +1000

    fixes import test_widget

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,20 @@
-from masks import get_mask_account, get_mask_card_number  # type: ignore
+from typing import Callable
+
+from src.masks import get_mask_account, get_mask_card_number # type: ignore
 
 
-def mask_account_card(user_card: str) -> None:
+def mask_account_card(user_card: str) -> str:
     """Функция маскировки карты или счета"""
     if len(user_card) <= 0:
         raise ValueError("Ошибка ввода! Пожалуйста, введите корректный номер карты или счета.")
     elif "Счет" in user_card:
         mask_acc_numb = f"{user_card[:4]} {get_mask_account(user_card[5:])}"
-        return print(mask_acc_numb)
+        return mask_acc_numb
     else:
         mask_cart_numb = f"{user_card[:-16]}{get_mask_card_number(user_card[-16:])}"
-        return print(mask_cart_numb)
+        return mask_cart_numb
 
 
-def get_date(user_date: str) -> None:
+def get_date(user_date: str) -> str:
     """Функция корректировки даты и возвращения оной в формате ДД.ММ.ГГГГ"""
-    return print(f"{user_date[8:10]}.{user_date[5:7]}.{user_date[:4]}")
+    return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[:4]}"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,19 +4,18 @@ from src.widget import mask_account_card, get_date
 
 
 @pytest.mark.parametrize(
-    "acc_num, expected",
+    "user_card, expected",
     [
         ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
         ("Счет 64686473678894779589", "Счет **9589"),
         ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
-        ("Счет 35383033474447895560", "Счет **5560"),
         ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
         ("Счет 73654108430135874305", "Счет **4305"),
     ],
 )
-def test_mask_account_card(acc_num, expected):
-    assert mask_account_card(acc_num) == expected
+def test_mask_account_card(user_card, expected):
+    assert mask_account_card(user_card) == expected
 
 
 def test_mask_invalid_account_card():
@@ -24,5 +23,13 @@ def test_mask_invalid_account_card():
         assert mask_account_card("")
 
 
-def test_get_date():
-    assert get_date("2024-03-22T02:26:18.671407") == "22.03.2024"
+@pytest.mark.parametrize(
+    "user_date, result",
+    [
+        ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+        ("2018-06-30T02:08:58.4255722", "30.06.2018"),
+    ]
+)
+def test_get_date(user_date, result):
+    assert get_date(user_date) == result
