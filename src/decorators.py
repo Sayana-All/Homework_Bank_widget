36de0d41a5d5d5be58bab050ddd5613012509a38
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-10 04:11:40 +1000

    testing files

--- src/decorators.py
+++ src/decorators.py
@@ -1,40 +1,38 @@
 from functools import wraps
-from typing import Callable, Optional, Any
 
 
-def log(filename: Optional[str]) -> Any:
+def log(filename):
     """Декоратор для логирования функции, её аргументов, результатов и ошибок"""
 
-    def logging_decorator(func: Callable) -> Any:
+    def logging_decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs) -> Any:
+        def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
                 if filename is not None:
                     with open(filename, "a") as file:
-                        file.write(f"\n{func.__name__} ok")
+                        file.write(f"{func.__name__} ok")
                 else:
-                    print(f"\n{func.__name__} ok")
+                    print(f"{func.__name__} ok")
                 return result
-            except Exception as err:
+            except Exception as e:
                 if filename is not None:
                     with open(filename, "a") as file:
-                        file.write(f"\n{func.__name__} error: {err}. Inputs: {args}, {kwargs}")
+                        file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
                 else:
-                    print(f"\n{func.__name__} error: {err}. Inputs: {args}, {kwargs}")
-                raise err
+                    print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                raise e
 
         return wrapper
 
     return logging_decorator
 
 
-@log(filename="mylog.txt")
-def my_function(x: int, y: int) -> int:
-    """Функция суммирует два числа и возвращает результат"""
-    return x + y
-
-
 if __name__ == "__main__":
-    my_function(2, 2)  # my_function ok
-    my_function(2, "2")  # my_function error: тип ошибки. Inputs: (1, "_"), {}
+    @log(filename="mylog.txt")
+    def my_function(x, y):
+        """Функция суммирует два числа и возвращает результат"""
+        return x + y
+
+
+    my_function(1, 2)
