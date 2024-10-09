a300f48121cb00b757d8759d90449bc1f958dc4e
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-10 00:46:06 +1000

    fixes homework files

--- src/decorators.py
+++ src/decorators.py
@@ -1,22 +1,23 @@
 from functools import wraps
-from typing import Optional, Callable
+from typing import Callable, Optional, Any
 
 
-def log(filename: Optional[str]):
+def log(filename: Optional[str]) -> Any:
     """Декоратор для логирования функции, её аргументов, результатов и ошибок"""
-    def logging_decorator(func):
+
+    def logging_decorator(func: Callable) -> Any:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args, **kwargs) -> Any:
             try:
                 result = func(*args, **kwargs)
-                if filename != None:
+                if filename is not None:
                     with open(filename, "a") as file:
                         file.write(f"\n{func.__name__} ok")
                 else:
                     print(f"\n{func.__name__} ok")
                 return result
             except Exception as err:
-                if filename != None:
+                if filename is not None:
                     with open(filename, "a") as file:
                         file.write(f"\n{func.__name__} error: {err}. Inputs: {args}, {kwargs}")
                 else:
@@ -29,11 +30,11 @@ def log(filename: Optional[str]):
 
 
 @log(filename="mylog.txt")
-def my_function(x, y):
+def my_function(x: int, y: int) -> int:
     """Функция суммирует два числа и возвращает результат"""
     return x + y
 
 
 if __name__ == "__main__":
-    my_function(2, 2) # my_function ok
-    my_function(2, "2") # my_function error: тип ошибки. Inputs: (1, "_"), {}
+    my_function(2, 2)  # my_function ok
+    my_function(2, "2")  # my_function error: тип ошибки. Inputs: (1, "_"), {}
