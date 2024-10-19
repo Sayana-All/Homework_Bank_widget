6dc5dcd0a9ad3e8666f259c6b1d85270891f2236
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-19 16:51:31 +1000

    add get_operations_gata func

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,26 @@
-from typing import Any
 import json
+from typing import Any
 
 
-def get_operations_data(file_path: str) -> list[dict[str, Any]]:
+def get_operations_data(file_path):
     """Обрабатывает JSON-файл и преобразует в словарь с транзакциями"""
+
     empty_data = []
     try:
-        with open(file_path) as file:
-            if len(file) == 0:
-                return empty_data
-            else:
+        with open(file_path, "r", encoding="utf-8") as file:
+            try:
                 operations = json.load(file)
+                if len(operations) == 0:
+                    return empty_data
                 return operations
-    except json.JSONDecodeError:
-        print("Ошибка выгрузки JSON-файла")
+            except json.JSONDecodeError:
+                print("Ошибка декодирования JSON-файла")
+                return empty_data
+    except FileNotFoundError:
+        print("Ошибка! Файл не найден")
+        return empty_data
 
 
-if __name__ == '__main__':
-    data = get_operations_data('\data\operations.json')
+if __name__ == "__main__":
+    data = get_operations_data(r"C:\Users\PC\PycharmProjects\Homework_Bank_widget\data\operations.json")
+    print(data)
