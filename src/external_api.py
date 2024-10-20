1f3e6d713ad3d910bdf3d046e1b83fd21356031d
anisa kazanbaeva <anisahime@yandex.ru>
2024-10-20 14:53:02 +1000

    add external_api module

---
+++ src/external_api.py
@@ -0,0 +1,24 @@
+from typing import Any
+import os
+from dotenv import load_dotenv
+import requests
+
+
+def convert_amount(currency_code: str, amount: str) -> Any:
+    """Конвертирует транзакции и возвращает сумму в рублях"""
+
+    try:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={amount}"
+        load_dotenv()
+        api_key = os.getenv("API_KEY")
+        headers = {"apikey": api_key}
+
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            print(f"Ошибка запроса. Возможная причина: {response.reason}")
+        else:
+            result = round(response.json()["result"], 2)
+            return result
+
+    except requests.exceptions.RequestException:
+        print("Произошла ошибка. Пожалуйста, повторите попытку позже.")
